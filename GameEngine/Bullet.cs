272df0d71b6fa08c6a972ec8f852788408fd3e5f
Joshua Watson <josh.m.watson@gmail.com>
2019-06-09 07:31:40 -0500

    added some baseline stuff

---
+++ GameEngine/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameEngine.GameObjects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GameEngine
+{
+    public class Bullet : GameObject
+    {
+        const float speed = 12f; //how fast bullet will move
+        Character owner;
+
+        int destroyTimer;
+        const int maxTimer = 180;
+
+        public Bullet() {
+            active = false;
+        }
+
+        public override void Load(ContentManager content)
+        {
+            image = TextureLoader.Load("bullet", content);
+            base.Load(content);
+        }
+
+        public override void Update(List<GameObject> objects, Map map)
+        {
+            if (!active)
+            {
+                return;
+            }
+
+            //Update movement
+            
+
+            base.Update(objects, map);
+        }
+    }
+}
